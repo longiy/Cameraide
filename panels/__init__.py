d8d2f3e2788ac5502bb88c3c87da12bca55db662
longiy <46914861+longiy@users.noreply.github.com>
2025-02-03 02:25:27 +0100

    remove main panel
    since coloraide is tied to a specific viewport it is better to have only sidepanel

--- panels/__init__.py
+++ panels/__init__.py
@@ -1,11 +1,9 @@
 # In panels/__init__.py
-from .main_panel import register as register_main_panel, unregister as unregister_main_panel
 from .sidebar_panel import register as register_sidebar_panel, unregister as unregister_sidebar_panel
 
 def register():
-    register_main_panel()
+
     register_sidebar_panel()
 
 def unregister():
     unregister_sidebar_panel()
-    unregister_main_panel()
